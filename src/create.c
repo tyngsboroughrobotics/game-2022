1d6bdbe4babd7266feb3d66317bf6f10f590539d
Will Riley <wjriley61406@icloud.com>
2022-01-27 15:21:55 -0500

    Setup1

--- src/create.c
+++ src/create.c
@@ -1,5 +1,30 @@
 #include <kipr/wombat.h>
 
+// Function Signitures 
+void out_box();
+void drive(double l_seconds, double r_seconds);
+
+const int basket = 0;
+
 int main() {
-    printf("Hello Me!\n");
+    printf("Starting Up...\n");
+    create_connect();
+    motor(basket, -100);
+    msleep(1000);
+    ao();
+    //out_box();
+    create_disconnect();
 }
+
+/*void out_box(){
+// msleep(5000);
+    drive()
+    msleep(2400);
+    create_stop();
+    create_disconnect();
+}
+
+void drive(double l_seconds, double r_seconds){
+    create_drive_direct(l_seconds*1000, r_seconds*1000);
+}
+*/
