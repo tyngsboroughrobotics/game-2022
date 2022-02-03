2c1a17c3747261c463515b19f7f68311d9c72d0b
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2022-02-03 11:47:21 -0500

    Update create

--- src/create.c
+++ src/create.c
@@ -1,30 +1,35 @@
+#if __has_include(<kipr/wombat.h>)
 #include <kipr/wombat.h>
+#else
+#error "Please compile on the Wombat"
+#endif
 
-// Function Signitures 
-void out_box();
-void drive(double l_seconds, double r_seconds);
+#include <stdio.h>
+#include <stdlib.h>
 
 const int basket = 0;
 
 int main() {
-    printf("Starting Up...\n");
-    create_connect();
-    motor(basket, -100);
-    msleep(1000);
-    ao();
-    //out_box();
-    create_disconnect();
+   printf("Starting Up...\n");
+   create_connect();
+   motor(basket, -100);
+   msleep(1000);
+   ao();
+   //out_box();
+   create_disconnect();
+
+   return 0;
 }
 
 /*void out_box(){
 // msleep(5000);
-    drive()
-    msleep(2400);
-    create_stop();
-    create_disconnect();
+   drive()
+   msleep(2400);
+   create_stop();
+   create_disconnect();
 }
 
 void drive(double l_seconds, double r_seconds){
-    create_drive_direct(l_seconds*1000, r_seconds*1000);
+   create_drive_direct(l_seconds*1000, r_seconds*1000);
 }
 */
