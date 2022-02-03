49ce36edf4f541e4e9205f1d5ab7a733d2b0501a
Will Riley <wjriley61406@icloud.com>
2022-02-03 15:34:24 -0500

    UpdateCreate

--- src/create.c
+++ src/create.c
@@ -9,30 +9,75 @@
 
 // Signatures
 void out_box();
-void drive(double l_seconds, double r_seconds);
+void drive(int l_velocity, int r_velocity, int duration);
+void open_basket_full();
+void close_basket_full();
+void open_basket_partway();
+void close_basket_partway();
+void move_motor(int port, int velocity, int duration);
+void turn_l(int degrees);
+void turn_r(int degrees);
 
+// Variables
 const int basket = 0;
+const int basket_full_open_duration = 8000;
+const int basket_partway_duration = 2000;
+const double ticks_per_degree = 4.25;
 
 int main() {
    printf("Starting Up...\n");
    create_connect();
-   motor(basket, -100);
-   msleep(1000);
-   ao();
-   //out_box();
+   out_box();
    create_disconnect();
 
    return 0;
 }
 
 void out_box(){
-    msleep(5000);
-    drive(1, 1);
-    msleep(2400);
-    create_stop();
+    int start_wait_time = 3000;
+    int time_to_cross = 900;
+
+    open_basket_partway();
+    msleep(start_wait_time);
+    drive(1000, 1000, time_to_cross);
+    turn_r(90);
+    drive(1000, 1000, 1400);
+    turn_l(95);
+    drive(1000, 1000, 300);
+    close_basket_partway();
     create_disconnect();
 }
 
-void drive(double l_seconds, double r_seconds){
-   create_drive_direct(l_seconds*1000, r_seconds*1000);
+void drive(int l_velocity, int r_velocity, int duration){
+   create_drive_direct(l_velocity, r_velocity);
+   msleep(duration);
+   create_stop();
+}
+
+void open_basket_full(){
+    move_motor(basket, -100, basket_full_open_duration);
+}
+void close_basket_full(){
+    move_motor(basket, 100, basket_full_open_duration);
+}
+
+void open_basket_partway(){
+    move_motor(basket, -100, basket_partway_duration);
+}
+void close_basket_partway(){
+    move_motor(basket, 100, basket_partway_duration);
+}
+
+void move_motor(int port, int velocity, int duration){
+    motor(port, velocity);
+    msleep(duration);
+    ao();
+}
+
+void turn_l(int degrees){
+    drive(-1000, 1000, (int) (degrees * ticks_per_degree));
+}
+
+void turn_r(int degrees){
+    drive(1000, -1000, (int) (degrees * ticks_per_degree));
 }
