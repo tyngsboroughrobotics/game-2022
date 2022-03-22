68d8488757e38c66415ccb8c9d41d08a5a10e9bd
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2022-03-22 14:32:57 -0400

    Keep trying to pull in the pom pom until it's secured in the shaft

--- src/demobot.py
+++ src/demobot.py
@@ -1,3 +1,4 @@
+import time
 import sys
 
 from lib.libwallaby import libwallaby
@@ -38,33 +39,23 @@ class Color(Enum):
 def main():
     libwallaby.camera_open()
     raise_arm()
-    # reset_splitter()
 
-    # initial_position_left = libwallaby.get_motor_position_counter(wheels.left_motor.port)
-    # initial_position_right = libwallaby.get_motor_position_counter(wheels.right_motor.port)
+    # TODO: Get in position
 
     colors = []
 
-    lower_arm()
-    enable_spinner()
+    def collect_poms():
+        lower_arm()
 
-    for _ in range(3):
-        drive_until_reached_pom()
+        for _ in range(3):
+            color = collect_pom()
 
-        pom_is_red = libwallaby.get_object_count(red_channel) > 0
-        advance_distance = cm(12)
+            if color:
+                colors.append(color)
 
-        if pom_is_red:
-            print("Red!"); sys.stdout.flush()
-            colors.append(Color.red)
-        else:
-            print("Green!"); sys.stdout.flush()
-            colors.append(Color.green)
+        raise_arm()
 
-        wheels.drive(Direction.forward, advance_distance)
-
-    disable_spinner()
-    raise_arm()
+    with_reset_wheels(collect_poms, padding=cm(15))
 
     print("COLORS:", colors)
 
@@ -72,15 +63,9 @@ def raise_arm():
     arm_servo.set(1260)
 
 def lower_arm():
-    arm_servo.set(1460)
+    arm_servo.set(1380)
 
-def enable_spinner():
-    libwallaby.motor(spinner_motor.port, 100)
-
-def disable_spinner():
-    libwallaby.off(spinner_motor.port)
-
-def drive_until_reached_pom():
+def collect_pom():
     for _ in range(10):
         libwallaby.camera_update()
 
@@ -90,6 +75,8 @@ def drive_until_reached_pom():
     libwallaby.move_at_velocity(wheels.left_motor.port, left_velocity)
     libwallaby.move_at_velocity(wheels.right_motor.port, right_velocity)
 
+    libwallaby.motor(spinner_motor.port, 100)
+
     while True:
         libwallaby.camera_update()
 
@@ -97,3 +84,36 @@ def drive_until_reached_pom():
             break
 
     wheels.force_stop()
+
+    wheels.drive(Direction.forward, cm(10))
+
+    # Keep trying to pull in the pom pom until it's secured in the shaft
+    while True:
+        libwallaby.camera_update()
+
+        if libwallaby.get_object_count(red_channel) > 0:
+            color = Color.red
+        elif libwallaby.get_object_count(green_channel) > 0:
+            color = Color.green
+        else:
+            break
+
+    libwallaby.off(spinner_motor.port)
+
+    return color
+
+def with_reset_wheels(f, padding = 0):
+    initial_position_left = libwallaby.get_motor_position_counter(wheels.left_motor.port)
+    initial_position_right = libwallaby.get_motor_position_counter(wheels.right_motor.port)
+
+    f()
+
+    final_position_left = libwallaby.get_motor_position_counter(wheels.left_motor.port)
+    final_position_right = libwallaby.get_motor_position_counter(wheels.right_motor.port)
+
+    left_distance = final_position_left - initial_position_left
+    right_distance = final_position_right - initial_position_right
+
+    distance = (left_distance if left_distance < right_distance else right_distance) / motor_pwm_ticks / motor_travel_time_1_cm
+
+    wheels.drive(Direction.reverse, distance - padding)
